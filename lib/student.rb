4ab87d9f549b054f1944be420a8f9aa5f22ce582
John Tuttle <thejwt@gmail.com>
2017-10-17 21:23:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,10 +8,10 @@ class Student < InteractiveRecord
     attributes.each do |property, value|
       self.send("#{property}=", value)
     end
+  end
 
-    self.column_names.each do |col_name|
-      attr_accessor col_name.to_sym
-    end
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
   end
 
   def self.table_name
